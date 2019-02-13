fe41da2873311dce072334ba54710c3f9faf8055
waelbasha <47597814+waelbasha@users.noreply.github.com>
2019-02-13 15:36:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+   client.user.setActivity("I Do Not Care",{type: 'WATCHING'})
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
+ 
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
@@ -262,6 +277,8 @@ client.on('ready', () => {
     client.user.setStatus("dnd")
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
 
 
 client.login("NTQ0OTU5NDc0NzYzNDMxOTc4.D0SxGQ.bTkfNVeD3WLHnwomofmGzDIcqo0");
